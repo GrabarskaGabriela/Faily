3fd1a3b4e06611e2dddcf55319e07ca18fcc891d
mateusz-bogacz-collegiumwitelona <mateusz.bogacz-drewniak@studenci.collegiumwitelona.pl>
2025-04-29 07:41:13 +0200

    dodanie maili z przypomnieniami o wydażeniach i odjazdach

--- src/routes/console.php
+++ src/routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('events:send-reminders')
+    ->dailyAt('10:00')
+    ->withoutOverlapping()
+    ->appendOutputTo(storage_path('logs/event-reminders.log'));
